c1d54e06cbdfb7633eebda8da24824bb421b73f7
Wilhelmich <99445453+Wilhelmich@users.noreply.github.com>
2023-08-16 23:10:01 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+YOUR_API_ID = '**************'
+YOUR_API_HASH = '****************'
+YOUR_PHONE_NUMBER = '***************'
+YOUR_GROUP_LINK = '****************'
+GROUP_ID = *************
+OPENAI_API_KEY = "***************"
+TOKEN_BOT = "***********
